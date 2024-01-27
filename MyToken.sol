ae69aec580fbb6a61ff04bf6e30dc03598623916
Sahitya Roy <72821604+SahityaRoy@users.noreply.github.com>
2024-01-27 21:15:00 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,12 @@
 
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RiseInCoin is ERC20 {
+    constructor() ERC20("RiseInCoin", "SAHITYA") {
+        _mint(msg.sender, 10000 *(10**18));
+    }
+}
